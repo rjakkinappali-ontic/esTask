2abea9be1841978d6ebb2e2b0334c72d7465aca6
rjakkinappali-ontic <rjakkinappali@ontic.co>
2022-03-15 18:54:15 +0530

    did all queries attempt 1

--- src/main/java/com/example/esBenchMarkingTask/controller/Controller.java
+++ src/main/java/com/example/esBenchMarkingTask/controller/Controller.java
@@ -1,11 +1,14 @@
 package com.example.esBenchMarkingTask.controller;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.example.esBenchMarkingTask.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,5 +20,13 @@ public class Controller {
     @GetMapping("/bulkWrite")
     public void bulkWriteDocs(@RequestBody String indexingType) {
         taskService.createIndex(indexingType);
+    }
+
+    @GetMapping("/query")
+    public long termQuery(@RequestBody JSONObject query) throws IOException {
+        long startTime = System.currentTimeMillis();
+        taskService.handleQuery(query);
+        long end = System.currentTimeMillis();
+        return end-startTime;
     }
 }
--- src/main/java/com/example/esBenchMarkingTask/service/TaskService.java
+++ src/main/java/com/example/esBenchMarkingTask/service/TaskService.java
@@ -1,15 +1,29 @@
 package com.example.esBenchMarkingTask.service;
 
-import com.example.esBenchMarkingTask.model.GeoPointTask;
-import com.example.esBenchMarkingTask.model.GeoShapeTask;
-import com.example.esBenchMarkingTask.model.TermQueryTask;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.example.esBenchMarkingTask.repository.RepoGeoPointQuery;
 import com.example.esBenchMarkingTask.repository.RepoGeoShapeQuery;
 import com.example.esBenchMarkingTask.repository.RepoTermQuery;
 import com.example.esBenchMarkingTask.utils.Util;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.geo.builders.EnvelopeBuilder;
+import org.elasticsearch.common.unit.DistanceUnit;
+import org.elasticsearch.index.query.GeoDistanceQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.locationtech.jts.geom.Coordinate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.RestClients;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -29,13 +43,100 @@ public class TaskService {
     private Util util;
 
     public void createIndex(String indexingType) {
-        switch(indexingType){
-            case "term":repoTermQuery.saveAll(util.getTermQueryTask());
-            break;
-            case "geo_shape":repoGeoShapeQuery.saveAll(util.getGeoShapeTask());
-            break;
-            case "geo_point":repoGeoPointQuery.saveAll(util.getGeoPointTask());
-            break;
+        switch (indexingType) {
+            case "term":
+                repoTermQuery.saveAll(util.getTermQueryTask());
+                break;
+            case "geo_shape":
+                repoGeoShapeQuery.saveAll(util.getGeoShapeTask());
+                break;
+            case "geo_point":
+                repoGeoPointQuery.saveAll(util.getGeoPointTask());
+                break;
+        }
+    }
+
+
+    public void handleQuery(JSONObject query) throws IOException {
+        String queryType = query.getString("type");
+        switch (queryType) {
+            case "term": handleTermQuery(query);
+                break;
+            case "geo_shape": handleGeoShapeQuery(query);
+                break;
+            case "geo_point": handleGeoPointQuery(query);
+                break;
+        }
+    }
+
+    private void handleGeoPointQuery(JSONObject query) throws IOException {
+        JSONArray locationArray = query.getJSONArray("Location");
+        List<Double> coordinates = new ArrayList<>();
+        for (int i=0; i<locationArray.size();i++){
+            coordinates.add(locationArray.getJSONObject(i).getDouble("X"));
+            coordinates.add(locationArray.getJSONObject(i).getDouble("Y"));
+        }
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        GeoDistanceQueryBuilder gdqb = new GeoDistanceQueryBuilder("Location");
+        gdqb.point(coordinates.get(0), coordinates.get(1));
+        gdqb.distance(20, DistanceUnit.KILOMETERS);
+        searchSourceBuilder.query(gdqb);
+        SearchRequest searchRequest = new SearchRequest();
+        searchRequest.indices("geopoint");
+        searchRequest.source(searchSourceBuilder);
+        ClientConfiguration clientConfiguration = ClientConfiguration.builder().connectedTo("localhost:9200").build();
+        RestHighLevelClient client = RestClients.create(clientConfiguration).rest();
+        SearchResponse response = client.search(searchRequest, RequestOptions.DEFAULT);
+        SearchHit[] searchHits = response.getHits().getHits();
+        System.out.println(searchHits.length);
+        for(SearchHit i:searchHits)
+        {
+            System.out.println(i.toString());
+        }
+    }
+
+    private void handleGeoShapeQuery(JSONObject query) throws IOException {
+        JSONArray locationArray = query.getJSONArray("Location");
+        List<Double> coordinates = new ArrayList<>();
+        for (int i=0; i<locationArray.size();i++){
+            coordinates.add(locationArray.getJSONObject(i).getDouble("X"));
+            coordinates.add(locationArray.getJSONObject(i).getDouble("Y"));
+        }
+        Coordinate coordinateTopLeft = new Coordinate(coordinates.get(0), coordinates.get(1));
+        Coordinate coordinateBottomRight = new Coordinate(coordinates.get(2), coordinates.get(3));
+        EnvelopeBuilder shape = new EnvelopeBuilder(coordinateTopLeft, coordinateBottomRight);
+        SearchSourceBuilder searchSourceBuilder= new SearchSourceBuilder();
+        searchSourceBuilder.query(QueryBuilders.geoWithinQuery("Location",shape));
+        SearchRequest searchRequest = new SearchRequest();
+        searchRequest.indices("geoshape");
+        searchRequest.source(searchSourceBuilder);
+        ClientConfiguration clientConfiguration = ClientConfiguration.builder().connectedTo("localhost:9200").build();
+        RestHighLevelClient client = RestClients.create(clientConfiguration).rest();
+        SearchResponse response = client.search(searchRequest, RequestOptions.DEFAULT);
+        SearchHit[] searchHits = response.getHits().getHits();
+        System.out.println(searchHits.length);
+        for(SearchHit i:searchHits)
+        {
+            System.out.println(i.toString());
+        }
+    }
+
+    private void handleTermQuery(JSONObject query) throws IOException {
+        String tileId = query.getString("tileId");
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        searchSourceBuilder.size(5);
+        searchSourceBuilder.query(QueryBuilders.termQuery("tileIds", tileId));
+        SearchRequest searchRequest = new SearchRequest();
+        searchRequest.indices("termquery");
+        searchRequest.source(searchSourceBuilder);
+        ClientConfiguration clientConfiguration = ClientConfiguration.builder().connectedTo("localhost:9200").build();
+        RestHighLevelClient client = RestClients.create(clientConfiguration).rest();
+        SearchResponse response = client.search(searchRequest, RequestOptions.DEFAULT);
+        SearchHit[] searchHits = response.getHits().getHits();
+        System.out.println(searchHits.length);
+        for(SearchHit i:searchHits)
+        {
+            System.out.println(i.toString());
         }
     }
 }
