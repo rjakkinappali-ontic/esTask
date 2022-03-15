b61c2a21c1dfb64ac2029d49374e49a5045b7cca
rjakkinappali-ontic <rjakkinappali@ontic.co>
2022-03-15 13:37:29 +0530

    first commit

---
+++ src/main/java/com/example/esBenchMarkingTask/EsBenchMarkingTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.esBenchMarkingTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsBenchMarkingTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsBenchMarkingTaskApplication.class, args);
+	}
+
+}
